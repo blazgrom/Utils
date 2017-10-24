17b29b41a778909782067b19b9090bb219b958d3
Slavi <slavi.angelov94@gmail.com>
2017-10-24 19:52:27 +0200

    Changed the name of the type container file and added always_false template class

--- type_container.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef TYPE_CONTAINER_HPP
-#define TYPE_CONTAINER_HPP
-namespace Utils
-{
-	//Class used for when we need to pass types as data to a function
-	template<class... Ts>
-	struct type_container{};
-}
-#endif // !TYPE_CONTAINER_HPP
