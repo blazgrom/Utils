85931b1cc8897e541d35258d482b80753746163c
Slavi <slavi.angelov94@gmail.com>
2017-11-25 17:11:19 +0100

    Run through clang-format

--- variadic_index.hpp
+++ variadic_index.hpp
@@ -2,20 +2,14 @@
 #define VARIADIC_INDEX_HPP
 namespace Utils
 {
-	template<class...Ts>
-	struct variadric_index
-	{
-		int get_index()
-		{
-			return  index++;
-		}
-		int get_reverse_index()
-		{
-			return reverse_index--;
-		}
-	private:
-		size_t reverse_index = sizeof...(Ts)-1;
-		size_t index = 0;
-	};
+template <class... Ts>
+struct variadric_index {
+    int get_index() { return index++; }
+    int get_reverse_index() { return reverse_index--; }
+
+  private:
+    size_t reverse_index = sizeof...(Ts) - 1;
+    size_t index = 0;
+};
 }
-#endif // !VARIADIC_INDEX_HPP
+#endif  // !VARIADIC_INDEX_HPP
