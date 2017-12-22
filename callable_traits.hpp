4eb2fc798dc9ec50874bb80a4a55f0daf1e9381f
blazgrom <slavi.angelov94@gmail.com>
2017-12-22 21:59:15 +0100

    Remove unnecessary code and renamed class to typename

--- callable_traits.hpp
+++ callable_traits.hpp
@@ -1,90 +1,74 @@
 #ifndef CALLABLE_TRAITS_HPP
 #define CALLABLE_TRAITS_HPP
 #include <functional>
+#include "has_operator_call.hpp"
 namespace Utils
 {
-template <class T>
-struct callable_traits_impl  // Intermediary for classes and lambdas
+template <typename T>
+struct callable_traits_impl  // Intermediary for typenamees and lambdas
 {
   private:
-    template <class R, class C = T, class... Args>
-    static constexpr auto getOperator(R (C::*op)(Args...))
+    template <typename R,
+              typename C = T,
+              typename... Args>
+    static constexpr auto get_operator_call(R (C::*op)(Args...))
     {
         return op;
     }
-
   public:
     using return_type = typename callable_traits_impl<decltype(
-        getOperator(&T::operator()))>::return_type;
+        get_operator_call(&T::operator()))>::return_type;
     using args = typename callable_traits_impl<decltype(
-        getOperator(&T::operator()))>::args;
+        get_operator_call(&T::operator()))>::args;
     static const size_t args_count =
-        callable_traits_impl<decltype(getOperator(&T::operator()))>::args_count;
+        callable_traits_impl<decltype(get_operator_call(&T::operator()))>::args_count;
 };
-template <class T>
-struct callable_traits_impl<const T>  // Intermediary for const classes and
-                                      // lambdas
-{
-  private:
-    template <class R, class C = T, class... Args>
-    static constexpr auto getConstOperator(R (C::*op)(Args...) const)
-    {
-        return op;
-    }
 
-  public:
-    using return_type = typename callable_traits_impl<decltype(
-        getConstOperator(&T::operator()))>::return_type;
-    using args = typename callable_traits_impl<decltype(
-        getConstOperator(&T::operator()))>::args;
-    static const size_t args_count = callable_traits_impl<decltype(
-        getConstOperator(&T::operator()))>::args_count;
-};
-template <class R, class... Args>
+template <typename R, typename... Args>
 struct callable_traits_impl<std::function<R(Args...)>> {
     using return_type = R;
     using args = std::tuple<Args...>;
     static const size_t args_count = sizeof...(Args);
 };
-template <class R, class... Args>
+template <typename R, typename... Args>
 struct callable_traits_impl<R (*)(Args...)> {
     using return_type = R;
     using args = std::tuple<Args...>;
     static const size_t args_count = sizeof...(Args);
 };
-template <class R, class C, class... Args>
+template <typename R, typename C, typename... Args>
 struct callable_traits_impl<R (C::*)(Args...) const volatile> {
     using return_type = R;
     using args = std::tuple<Args...>;
     static const size_t args_count = sizeof...(Args);
 };
-template <class R, class C, class... Args>
+template <typename R, typename C, typename... Args>
 struct callable_traits_impl<R (C::*)(Args...) const> {
     using return_type = R;
     using args = std::tuple<Args...>;
     static const size_t args_count = sizeof...(Args);
 };
-template <class R, class C, class... Args>
+template <typename R, typename C, typename... Args>
 struct callable_traits_impl<R (C::*)(Args...) volatile> {
     using return_type = R;
     using args = std::tuple<Args...>;
     static const size_t args_count = sizeof...(Args);
 };
-template <class R, class C, class... Args>
+template <typename R, typename C, typename... Args>
 struct callable_traits_impl<R (C::*)(Args...)> {
     using return_type = R;
     using args = std::tuple<Args...>;
     static const size_t args_count = sizeof...(Args);
 };
-template <class T>
+template <typename T>
 struct callable_traits {
     using return_type = typename callable_traits_impl<T>::return_type;
     using args = typename callable_traits_impl<T>::args;
     static const size_t args_count = callable_traits_impl<T>::args_count;
 };
-template <class T>
+template <typename T>
 using callable_return_type = typename callable_traits<T>::return_type;
-template <class T>
+template <typename T>
 using callable_arg_types = typename callable_traits<T>::args;
 }
 #endif  // !CALLABLE_TRAITS_HPP
